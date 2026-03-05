886592905536f0eafb765a7b6a5bb7158042b43a
AnantDhavale <47012267+AnantDhavale@users.noreply.github.com>
2026-03-05 17:08:59 +0530

    Rename package from cerone to cerone_SDK

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = ROOT / "README.md"
 os.chdir(ROOT)
 
 setup(
-    name="cerone",
+    name="cerone_SDK",
     version="0.2.0",
     author="Anant Dhavale for Homer Semantics",
     author_email="info@homersemantics.com",
