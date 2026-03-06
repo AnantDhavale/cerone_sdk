d2bbf4ad5e4affde424720376a50403ec6b46d11
AnantDhavale <47012267+AnantDhavale@users.noreply.github.com>
2026-03-06 14:31:43 +0530

    Bump version from 0.2.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(ROOT)
 
 setup(
     name="cerone",
-    version="0.2.0",
+    version="1.0.0",
     author="Anant Dhavale for Homer Semantics",
     author_email="info@homersemantics.com",
     description="Zero Trust Security for AI Agents",
