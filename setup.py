e35629a57144a282ad4742fdabacd576689173b9
AnantDhavale <47012267+AnantDhavale@users.noreply.github.com>
2026-05-20 13:05:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(ROOT)
 
 setup(
     name="cerone",
-    version="1.1.14",
+    version="1.1.15",
     author="Anant Dhavale for Homer Semantics",
     author_email="info@homersemantics.com",
     description="Zero Trust Security for AI Agents",
